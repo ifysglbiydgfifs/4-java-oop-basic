33ce6f6b57826401be0e69d09538f3796aa191b6
ifysglbiydgfifs <igbsh43hsdh@gmail.com>
2024-10-18 00:12:33 +0500

    Мустафьев Никита При-201. Task05

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,17 +1,29 @@
 package com.example.task05;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
+    public ArrayList<Point> PointsList;
+    public ArrayList<Point> newPointsList;
+
+    public PolygonalLine() {
+        PointsList = new ArrayList<>();
+        newPointsList = new ArrayList<>();
+    }
 
     /**
      * Устанавливает точки ломаной линии
      *
-     * @param points массив точек, которыми нужно проинициализировать ломаную линию
+     * @param pointsArray массив точек, которыми нужно проинициализировать ломаную линию
      */
-    public void setPoints(Point[] points) {
-        // TODO: реализовать
+
+    public void setPoints(Point[] pointsArray) {
+        Collections.addAll(PointsList, pointsArray);
+        Collections.addAll(newPointsList, pointsArray);
     }
 
     /**
@@ -20,7 +32,7 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        newPointsList.add(point);
     }
 
     /**
@@ -30,7 +42,8 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        Point newPoint = new Point(x,y);
+        newPointsList.add(newPoint);
     }
 
     /**
@@ -39,8 +52,16 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
-    }
+        double totalLength = 0;
+        for (int i=0;i<PointsList.size()-1;i++)
+        {
+            Point currentPoint = PointsList.get(i);
+            Point nextPoint = PointsList.get(i+1);
 
+            totalLength+=currentPoint.getLength(nextPoint);
+        }
+        System.out.println(totalLength);
+        return totalLength;
+
+    }
 }
